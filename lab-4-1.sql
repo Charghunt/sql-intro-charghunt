1d69764fcdb900e3063429d4cf0cf49ff7832d4c
Charlotte Gunn Hunt <charlotte.gunn@kellogg.northwestern.edu>
2023-04-06 02:21:51 +0000

    end of session

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -4,3 +4,14 @@
 -- 2935
 
 
+SELECT COUNT (id)
+FROM teams;
+
+
+SELECT SUM (stats.hits) 
+FROM players INNER JOIN stats ON players.id = stats.player_id
+WHERE players.last_name = "Bonds";
+
+SELECT * From stats limit 10; 
+
+--Do the other exercises at home!--
